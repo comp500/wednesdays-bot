317c96fb9946fcf54f1f0d3ea9f4a93c83f0873e
comp500 <comp500@users.noreply.github.com>
2017-12-09 20:08:38 +0000

    Add ytrand

---
+++ commands/ytrand.js
@@ -0,0 +1,36 @@
+let youtube;
+module.exports = {
+	commands: ["ytrand", "youtuberand"],
+	onReady: (client, globalYoutube) => {
+		youtube = globalYoutube;
+	},
+	onMsg: (inputs, msg) => {
+		if (youtube.existingVideoIDs.length > 0) {
+			if ((new Date() - youtube.lastRequestTime) > (1000 * 60)) { // 60 seconds between requests
+				youtube.updateList((err) => {
+					if (err) {
+						/* eslint-disable no-console */
+						console.error(err);
+						/* eslint-enable no-console */
+						msg.reply("There was an error retrieving the playlist.");
+					} else if (youtube.existingVideoIDs.length > 0) {
+						msg.reply("https://youtube.com/watch?v=" + youtube.existingVideoIDs[Math.floor(Math.random() * youtube.existingVideoIDs.length)]);
+					}
+				}, true);
+			} else {
+				msg.reply("https://youtube.com/watch?v=" + youtube.existingVideoIDs[Math.floor(Math.random() * youtube.existingVideoIDs.length)]);
+			}
+		} else {
+			youtube.updateList((err) => {
+				if (err) {
+					/* eslint-disable no-console */
+					console.error(err);
+					/* eslint-enable no-console */
+					msg.reply("There was an error retrieving the playlist.");
+				} else if (youtube.existingVideoIDs.length > 0) {
+					msg.reply("https://youtube.com/watch?v=" + youtube.existingVideoIDs[Math.floor(Math.random() * youtube.existingVideoIDs.length)]);
+				}
+			}, true);
+		}
+	}
+};
