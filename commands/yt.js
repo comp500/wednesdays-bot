41a46d4c110f0d05d71eb114bbd05075f922d1f8
comp500 <comp500@users.noreply.github.com>
2017-12-09 17:13:07 +0000

    Add yt api, and test yt api

--- commands/yt.js
+++ commands/yt.js
@@ -1,6 +1,21 @@
+const YouTube = require("youtube-node");
+const youtube = new YouTube();
+youtube.setKey(require("../tokens.json").youtube);
+
 module.exports = {
-	commands: ["yt"],
+	commands: ["yt", "youtube", "zimonitrome"],
 	onMsg: (inputs, msg) => {
-		msg.reply("Not implemented yet.");
+		youtube.getPlayListsItemsById(require("./strings.json").wednesdayList, null, (err, result) => {
+			if (err) {
+				/* eslint-disable no-console */
+				console.error(err);
+				/* eslint-enable no-console */
+				msg.reply("There was an error retrieving the playlist.");
+			} else {
+				/* eslint-disable no-console */
+				console.log(JSON.stringify(result));
+				msg.reply("Not implemented yet.");
+			}
+		});
 	}
 };
