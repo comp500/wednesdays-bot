74be8a71fdfa4374f709cb2686ff9b3f79ecd8d3
comp500 <comp500@users.noreply.github.com>
2017-12-10 16:52:51 +0000

    Push command

---
+++ commands/push.js
@@ -0,0 +1,29 @@
+let store;
+const strings = require("../strings.json");
+const tokens = require("../tokens.json");
+module.exports = {
+	commands: ["subscribe"],
+	onReady: (client, modulesList) => {
+		store = modulesList["_store.js"];
+	},
+	onMsg: (inputs, msg) => {
+		if (msg.author.id == tokens.ownerid) {
+			let internalStore = store.getStore();
+			if (internalStore.subscriptions) {
+				internalStore.subscriptions.forEach((element) => {
+					let channel = msg.client.channels.get(element.channel);
+					if (channel) {
+						channel.send(inputs.join(" "));
+					} else {
+						msg.reply(strings.push.fail + element.channel);
+					}
+				});
+				msg.reply(strings.push.success);
+			} else {
+				msg.reply(strings.push.nosub);
+			}
+		} else {
+			msg.reply(strings.permission);
+		}
+	}
+};
