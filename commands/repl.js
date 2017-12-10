e693ed01078c3c1efbdad9202791ae46bba1d062
comp500 <comp500@users.noreply.github.com>
2017-12-10 14:33:20 +0000

    Improved inspect, move requires to top

--- commands/repl.js
+++ commands/repl.js
@@ -1,17 +1,41 @@
 const util = require("util");
+const strings = require("../strings.json");
+const tokens = require("../tokens.json");
 let client;
 let replEnabled = false;
 let replChannel = null;
 let selfInstance = {
 	commands: ["repl"],
+	superInspect: function (obj) {
+		let output = util.inspect(obj);
+		if (output.length < 1000) {
+			return output;
+		} else {
+			for (let i = 0; i < 100; i++) {
+				output = util.inspect(obj);
+				if (output.length < 1000) {
+					break;
+				}
+			}
+			if (output.length > 1000) {
+				return strings.repl.toolarge;
+			} else {
+				return output;
+			}
+		}
+	},
 	onREPL: function (msg) {
 		if (replChannel == null || !replEnabled) {
 			client.removeListener("message", selfInstance.onREPL);
 		}
-		if (msg.author.id == require("../tokens.json")["ownerid"]) {
+		if (msg.author.id == tokens.ownerid) {
 			if (msg.channel.id == replChannel) {
-				let output = eval(msg.content);
-				msg.reply("`> " + util.inspect(output) + "`");
+				try {
+					let output = eval(msg.content);
+					msg.reply("`> " + selfInstance.superInspect(output) + "`");
+				} catch (e) {
+					msg.reply(strings.update.error + "```" + e + "```");
+				}
 			}
 		}
 	},
@@ -19,20 +43,20 @@ let selfInstance = {
 		client = globalClient;
 	},
 	onMsg: function (inputs, msg) {
-		if (msg.author.id == require("../tokens.json")["ownerid"]) {
+		if (msg.author.id == tokens["ownerid"]) {
 			if (replEnabled) {
 				client.removeListener("message", selfInstance.onREPL);
 				replEnabled = false;
 				replChannel = null;
-				msg.reply("REPL disabled!");
+				msg.reply(strings.repl.disabled);
 			} else {
 				replEnabled = true;
 				replChannel = msg.channel.id;
 				client.on("message", selfInstance.onREPL);
-				msg.reply("REPL enabled!");
+				msg.reply(strings.repl.enabled);
 			}
 		} else {
-			msg.reply("You don't have permission to do that!");
+			msg.reply(strings.permission);
 		}
 	}
 };
