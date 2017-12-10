0c2ad5444ad6f147c794bef40fb3364ffa7549fa
comp500 <comp500@users.noreply.github.com>
2017-12-10 21:43:09 +0000

    Don't crash on only 1 input

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ client.on("message", msg => {
 	if (msg.content.trim().slice(0, 9).toLowerCase() === "wednesday") {
 		let inputs = msg.content.trim().split(" ");
 		inputs.filter((input) => input.trim().length > 0);
-
-		if (commandKeys.includes(inputs[1].toLowerCase())) {
-			commandsList[inputs[1].toLowerCase()](inputs.slice(2), msg, client);
+		
+		if (inputs.length > 1) {
+			if (commandKeys.includes(inputs[1].toLowerCase())) {
+				commandsList[inputs[1].toLowerCase()](inputs.slice(2), msg, client);
+			}
 		}
 	}
 });
