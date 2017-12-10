0b4c2d55ea442288c2caf53740536c7fb8b6677e
comp500 <comp500@users.noreply.github.com>
2017-12-10 22:45:05 +0000

    Add subscriptions to stats

--- commands/stats.js
+++ commands/stats.js
@@ -1,9 +1,17 @@
 const strings = require("../strings.json");
+let store;
 module.exports = {
 	commands: ["stats"],
+	onReady: (client, modulesList) => {
+		store = modulesList["_store.js"];
+	},
 	onMsg: (inputs, msg) => {
 		let output = "";
 		output += strings.stats.guilds + msg.client.guilds.array().length + "\n";
+		let subscriptions = store.readKey("subscriptions");
+		if (subscriptions) {
+			output += strings.stats.subbed + subscriptions.length + "\n";
+		}
 		output += strings.stats.ping + msg.client.ping + "ms\n";
 		output += strings.stats.uptime + (msg.client.uptime / 1000) + "s\n";
 		output += strings.stats.platform + process.arch + " " + process.platform + "\n";
