73ea63ce9eaaae5378f0b2640a745e803c106ae4
comp500 <comp500@users.noreply.github.com>
2017-12-10 22:37:44 +0000

    Fix notifier to actually do stuff

--- commands/_notifier.js
+++ commands/_notifier.js
@@ -1,12 +1,27 @@
 let client;
+let store;
 module.exports = {
-	onReady: (globalClient) => {
+	onReady: (globalClient, modulesList) => {
 		client = globalClient;
+		store = modulesList["_store.js"];
 	},
 	notify: (videoId, excludeChannel) => {
-		/* eslint-disable no-console */
-		console.log("notify", videoId, excludeChannel);
-		/* eslint-enable no-console */
-		client; // ¯\_(ツ)_/¯
+		let internalStore = store.getStore();
+		if (internalStore.subscriptions) {
+			internalStore.subscriptions.forEach((element) => {
+				let channel = client.channels.get(element.channel);
+				if (channel) {
+					if (channel.id != excludeChannel) {
+						channel.send("https://youtube.com/watch?v=" + videoId);
+					}
+				} else {
+					console.log("Failed to push to " + channel.id);
+				}
+			});
+			console.log("Successfully pushed " + videoId);
+		} else {
+			console.error("No subscriptions to notify, is the datastore broken?");
+		}
 	}
+	// TODO: do this on interval
 };
